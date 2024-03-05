6c0ca4b0bb801fca8ac6d24094d432298fdcbbc6
wyllamys94 <wyllamysynwram@gmail.com>
2024-03-05 19:52:03 -0300

    alteração no valaor do input

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 function generateNumber() {
-   
+
     const min = Math.ceil(document.querySelector(".input-min").value)
-    const max =  Math.floor(document.querySelector(".input-max").value)
+    const max = Math.floor(document.querySelector(".input-max").value)
+   
+    if (min >= max) 
+    { alert("O valor minimo tem quer ser MENOR que o valor máximo") }
 
-    const result = Math.floor(Math.random() * (max - min + 1) + min);
+    else {
+        const result = Math.floor(Math.random() * (max - min + 1) + min);
+        alert(result)
+    }
 
-    alert (result)
 
 }
